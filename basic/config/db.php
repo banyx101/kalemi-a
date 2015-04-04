0dbb285863c2aaac3126066f0a34fbe56e3ace2a
Fabjan Lashi <kaligrafik@gmail.com>
2015-04-04 22:03:42 +0200

    mnk

--- basic/config/db.php
+++ basic/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=kalemi',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'kalemi',
+    'password' => 'K4l3m1',
     'charset' => 'utf8',
 ];
